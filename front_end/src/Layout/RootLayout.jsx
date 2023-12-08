e02cacbf03a6408919217786560d1c5843a17cf1
Phạm Minh Hiếu 21020319 <21020319@vnu.edu.vn>
2023-12-08 22:42:48 +0700

    navbar

--- front_end/src/Layout/RootLayout.jsx
+++ front_end/src/Layout/RootLayout.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Sidebar from './Sidebar/Sidebar'
+import Navbar from './Navbar/Navbar'
 
 function RootLayout({children}) {
   return (
-    <div className='flex gap-5'>
+    <div className='flex '>
       <Sidebar />
-      <main className='max-w-5xl flex-1 mx-auto py-4'>{children}</main>
+      <div className='w-full'>
+        <Navbar className=""/>
+        <main className='max-w-5xl flex-4 mx-auto py-4'>{children}</main>
+      </div>
     </div>
   )
 }
